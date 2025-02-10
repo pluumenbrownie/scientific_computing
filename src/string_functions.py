830da1acb09bed1d90d1cc3ef75229add0b77590
pluumenbrownie <git.wessel@beumertjes.nl>
2025-02-10 14:35:12 +0100

    created string_viewer for #19

---
+++ src/string_functions.py
@@ -0,0 +1,51 @@
+import taichi as ti 
+import taichi.math as tm
+
+
+ti.init()
+
+
+# init boilerplate
+N: int = 1_000
+dx: float = 1/N
+amplitude = ti.field(float, shape = N)
+amplitude_previous = ti.field(float, shape = N)
+
+
+@ti.kernel
+def init_string_1():
+    """
+    Psi(x, t=0) = sin(2 pi x)
+    """
+    for i in range(N):
+        amplitude[i] = tm.sin(2 * tm.pi * i * dx)
+        amplitude_previous[i] = tm.sin(2 * tm.pi * i * dx)
+
+
+@ti.kernel
+def init_string_2():
+    """
+    Psi(x, t=0) = sin(5 pi x)
+    """
+    for i in range(N):
+        amplitude[i] = tm.sin(5 * tm.pi * i * dx)
+        amplitude_previous[i] = tm.sin(5 * tm.pi * i * dx)
+
+
+@ti.kernel
+def init_string_3():
+    """
+    Psi(x, t=0) = sin(5 pi x) if 1/5 < x < 2/5
+    """
+    for i in range(N):
+        if i * dx > 1/5.0 and i*dx < 2/5:
+            amplitude[i] = tm.sin(5 * tm.pi * i * dx)
+            amplitude_previous[i] = tm.sin(5 * tm.pi * i * dx)
+        else:
+            amplitude[i] = tm.sin(0.0)
+            amplitude_previous[i] = tm.sin(0.0)
+
+
+@ti.kernel
+def update_string():
+    pass
---
+++ src/string_viewer.py
@@ -0,0 +1,41 @@
+import taichi as ti 
+import taichi.math as tm
+from string_functions import init_string_1, init_string_2, init_string_3, update_string
+
+
+ti.init()
+
+
+# init boilerplate
+N: int = 1_000
+dx: float = 1/N
+amplitude = ti.field(float, shape = N)
+amplitude_previous = ti.field(float, shape = N)
+
+# for gui
+resolution = (512, 256)
+pixels = ti.field(float, shape=resolution)
+
+
+@ti.kernel
+def update_gui():
+    float_N = float(N)
+    for i, j in pixels:
+        pixels[i, j] = 1
+    for i in amplitude:
+        i_pixel = tm.round(i / float_N * resolution[0], dtype=int)
+        j_pixel = tm.round(amplitude[i] * 128 + 128, dtype=int)
+        pixels[i_pixel, j_pixel] = 0
+        
+
+init_string_3()
+update_string()
+        
+gui = ti.GUI("Vibrating String", res=resolution)
+
+i = 0
+while gui.running:  
+    update_gui()
+    gui.set_image(pixels)
+    gui.show()
+    i += 1
