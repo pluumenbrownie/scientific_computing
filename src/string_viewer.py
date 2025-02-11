20cbcab41e6349cf627ccf97ad4c4ab2f6ba406d
pluumenbrownie <git.wessel@beumertjes.nl>
2025-02-11 14:31:43 +0100

    added speed controlls to save_video

--- src/string_viewer.py
+++ src/string_viewer.py
@@ -1,5 +1,5 @@
+from typing import Callable
 import taichi as ti
-import taichi.math as tm
 import numpy as np
 import os
 from string_functions import init_string_1, init_string_2, init_string_3, update_string
@@ -21,36 +21,45 @@ resolution = (512, 256)
 
 
 def update_gui(gui: ti.GUI):
+    """
+    Clears the current gui viewport and draws the string as a series of lines.
+    """
     wave_points = get_wave_points()
     gui.clear(0xFFFFFF)
     gui.lines(wave_points[:-1], wave_points[1:], radius=1, color=0xFF0000)
 
 
-def save_video(frames: int, location: str = "./local"):
-    init_string_3()
+def save_video(
+    frames: int,
+    init_fn: Callable,
+    location: str = "./local",
+    step_size: int = 1,
+    fps: int = 60,
+):
+    init_fn()
     video_manager = ti.tools.VideoManager(
         output_dir=location,
-        framerate=24,
+        framerate=fps,
         automatic_build=False,
         video_filename="test_string",
     )
     gui = ti.GUI("Saving private String", res=resolution, show_gui=False)  # type:ignore
     for _ in range(frames):
-        update_string()
-        update_gui(gui)
+        for _ in range(step_size):
+            update_string()
+            update_gui(gui)
         video_manager.write_frame(gui.get_image())
 
     video_manager.make_video(mp4=True, gif=False)
     # video_manager.clean_frames() is known to be broken https://github.com/taichi-dev/taichi/issues/6936
     frame_folder = location + "/frames"
     for fn in os.listdir(frame_folder):
-        if fn.endswith('.png'):
-            os.remove(frame_folder + '/' + fn) 
+        if fn.endswith(".png"):
+            os.remove(frame_folder + "/" + fn)
     os.rmdir(frame_folder)
 
 
 def get_wave_points():
-    width, height = resolution
     x_coords = np.linspace(0, 1, N)
     y_coords = 0.5 + amplitude.to_numpy() * 0.2
     return np.stack([x_coords, y_coords], axis=1)
@@ -68,4 +77,4 @@ def run_gui():
 
 if __name__ == "__main__":
     # run_gui()
-    save_video(100)
+    save_video(400, init_string_1, step_size=4)
