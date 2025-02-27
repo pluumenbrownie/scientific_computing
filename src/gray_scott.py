1b27a2c8aea2e13f6822d3a817061a0bc81edc3f
pluumenbrownie <git.wessel@beumertjes.nl>
2025-02-27 18:41:32 +0100

    trying to debug a weird bug in how fields work

--- src/gray_scott.py
+++ src/gray_scott.py
@@ -15,19 +15,14 @@ F = 0.035
 K = 0.060
 
 
-@ti.dataclass
-class UV:
-    u: float
-    v: float
-
-
 @ti.data_oriented
 class GrayScott:
     def __init__(self, size: int) -> None:
-        self.diff_const = UV((DT * DU) / (DX**2), (DT * DV) / (DX**2))
+        self.diff_const = ti.types.vector([(DT * DU) / (DX**2), (DT * DV) / (DX**2)])
         self.size = size
 
-        self.concentrations = UV.field(shape=(size, size))
+        self.concentrations = ti.Vector.field(size, dtype=float, shape=(size, size))
+        self.previous = ti.Vector.field(size, dtype=float, shape=(size, size))
 
     def init_concentration(
         self,
@@ -60,46 +55,84 @@ class GrayScott:
     @ti.kernel
     def _initialize_seperate(
         self,
-        u_concentration: ti.types.ndarray(dtype=ti.f32, ndim=2),
-        v_concentration: ti.types.ndarray(dtype=ti.f32, ndim=2),
+        u_concentration: ti.types.ndarray(dtype=ti.f32, ndim=2),  # type: ignore
+        v_concentration: ti.types.ndarray(dtype=ti.f32, ndim=2),  # type: ignore
     ):
         for i, j in self.concentrations:
-            self.concentrations[i, j].u = u_concentration[i, j]
-            self.concentrations[i, j].v = v_concentration[i, j]
+            self.concentrations[i, j][0] = u_concentration[i, j]
+            self.concentrations[i, j][1] = v_concentration[i, j]
+        print(u_concentration[0, 0])
 
     @ti.kernel
     def _initialize_combined(
-        self,
-        concentration: ti.types.ndarray(dtype=ti.f32, ndim=3),
+        self, concentration: ti.types.ndarray(dtype=ti.f32, ndim=3)  # type: ignore
     ):
         for i, j in self.concentrations:
-            self.concentrations[i, j].u = concentration[i, j, 0]
-            self.concentrations[i, j].v = concentration[i, j, 1]
+            self.concentrations[i, j][0] = concentration[i, j, 0]
+            self.concentrations[i, j][1] = concentration[i, j, 1]
+
+    @ti.func
+    def bc(self, i: int):
+        """
+        Periodic boundary conditions.
+        """
+        return i % self.size
+
+    @ti.func
+    def copy_into_previous(self):
+        for i, j in self.concentrations:
+            self.previous[i, j] = self.concentrations[i, j]
+
+    @ti.func
+    def diffuse(self, i: int, j: int):
+        # aliases to make formula a (little) bit clearer
+        bc, c = ti.static(self.bc, self.previous)
+        neighbours = (
+            c[bc(i + 1), bc(j)]
+            + c[bc(i - 1), bc(j)]
+            + c[bc(i), bc(j + 1)]
+            + c[bc(i), bc(j - 1)]
+            - 4 * c[i, j]
+        )
+        # neighbours.outer_product(self.diff_const)
+        # neighbours[0] *= self.diff_const[0]
+        # neighbours[1] *= self.diff_const[1]
+        # return neighbours
+
+    @ti.kernel
+    def step_diffusion(self):
+        self.copy_into_previous()
+
+        for i, j in self.concentrations:
+            self.diffuse(i, j)
+            # self.concentrations[i, j] += self.diffuse(i, j)
+            # self.concentrations[i, j][1] = self.diffuse(i, j, 1)
 
     @ti.kernel
     def draw(self, scale: int):
         for i, j in self.display:
-            self.display[i, j].r = ti.min(
-                self.concentrations[i // scale, j // scale].u * 256, 255
+            self.display[i, j][0] = ti.min(
+                self.concentrations[i // scale, j // scale][0] * 256, 255
             )
-            self.display[i, j].g = ti.min(
-                self.concentrations[i // scale, j // scale].v * 256, 255
+            self.display[i, j][1] = ti.min(
+                self.concentrations[i // scale, j // scale][1] * 256, 255
             )
-            self.display[i, j].b = 0
+            self.display[i, j][2] = 0
 
     def gui_loop(self, scale: int = 1):
         scaled_size = self.size * scale
-        gui = ti.GUI("Gray-Scott Reaction-Diffusion", res=(scaled_size, scaled_size))  # type: ignore
+        gui = ti.GUI("Gray-Scott Reaction-Diffusion", res=scaled_size)
         self.display = ti.Vector.field(3, ti.u8, shape=(scaled_size, scaled_size))
 
         while gui.running:
+            self.step_diffusion()
             self.draw(scale)
             gui.set_image(self.display)
             gui.show()
 
 
 if __name__ == "__main__":
-    N = 100
+    N = 5
     gray_scott = GrayScott(N)
     u_concentration = np.full((N, N), 0.5, dtype=np.float32)
     v_concentration = np.zeros_like(u_concentration)
@@ -107,4 +140,4 @@ if __name__ == "__main__":
     gray_scott.init_concentration(
         u_concentration=u_concentration, v_concentration=v_concentration
     )
-    gray_scott.gui_loop(scale=4)
+    gray_scott.gui_loop(scale=40)
